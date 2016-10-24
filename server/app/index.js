544086c0944a99fecf74b68924de473576ea701d
Victor Martin <vmartin@axway.com>
2016-10-24 17:07:13 +0100

    Add user json file and basic routes

---
+++ server/app/index.js
@@ -0,0 +1,29 @@
+var app = require('express')();
+var fs = require('fs');
+
+var users = [];
+
+fs.readFile('app/users.json', 'utf8', (err, data) => {
+  if (err) throw err;
+  JSON.parse(data).forEach((user) => {
+    users.push(user);
+  });
+});
+
+app.get('/', (req, res) => {
+  res.send('Hello world!\n');
+});
+
+app.get('/user', (req, res) => {
+  res.json(users);
+});
+
+app.get('/user/:username', function (req, res) {
+  var username = req.params.username;
+  var user = users.filter((user) => user.username === username);
+  res.json(user);
+})
+
+var server = app.listen(4000, () => {
+  console.log('Server listening on port ', server.address().port);
+});
